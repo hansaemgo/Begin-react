bc561c25c7cbadd8b70743c5e16905327954dbf7
hansaemgo <rhgkstoa7@gmail.com>
2020-01-23 16:42:07 +0900

    JSX 문법 - 규칙/ style/class/주석

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Hello from './hello';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Hello/>
-      <Hello/>
-
-    </div>
+  const name = 'hansaemgo';
+  const style = {
+    backgroundColor : 'black',
+    color: 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  };
+  return ( 
+    <>                 
+      <div className="App">
+        <Hello/>
+        {/*중괄호로 감싸서 변수의 값 보여주기 */}
+        <div style={style} >{name}</div>   
+        <div className="gray-box"></div> 
+      </div>
+    </>
   );
 }
 
