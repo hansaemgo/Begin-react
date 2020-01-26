31f9945167a60dfbb2eb6e17830e6f97640d71ad
hansaemgo <rhgkstoa7@gmail.com>
2020-01-26 19:44:32 +0900

    배열에 항목추가하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 
 
 function App() {
+
+  const [inputs, setInputs] = useSteate({
+    username : '',
+    email: ''
+  })
+
+  const { username, email } = inputs;
+  
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+  } ;
+
   const users = [
     {
         id : 1,
@@ -25,6 +42,11 @@ function App() {
   const nextId = useRef(4);
 
   const oncreate = () => {
+    setinpus ({
+        username : '',
+        email: ''
+
+    });
 
     console.log(nextId.current); // 4 
     nextId.current += 1;
@@ -32,7 +54,15 @@ function App() {
   }
 
   return (    
-   <UserList users={users}/>
+    <>
+    <CreateUser
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
+    <UserList users={users}/>
+    </>
   )
 }
 
---
+++ src/CreateUser.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// onChange - input값 바뀔때 처리되는 함수
+// onCreate - 버튼을 눌렀을때 새로운 항목 등록해주는 함수
+function CreateUser ({ username, email, onChange, onCreate}) {
+    return(
+        <div>
+            <input 
+                name="username" 
+                placeholder="계정명"
+                onChange="{onChange}"
+                value={username}    
+            />
+            <input
+                name="email" 
+                placeholder="이메일"
+                onChange="{onChange}"
+                value={email}   
+            />
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+
+export default CreateUser;
